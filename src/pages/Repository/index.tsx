309f39a6c7214bd84de90c19a9354c1100bf373b
jeffersoncharlles <contato@jefferdeveloper.com>
2022-03-18 19:01:44 -0300

    chore: deploy ok gitpages commit

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -86,7 +86,7 @@ export const Repository = () => {
     }, [page, repository, filters])
 
     const handleBack = () => {
-        navigation('/');
+        navigation(-1);
     }
 
     const handlePage = (action: string) => {
