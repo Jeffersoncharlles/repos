37a376a8dbf9be20f3547d62c058cb801104a41b
jeffersoncharlles <contato@jefferdeveloper.com>
2022-03-18 18:05:33 -0300

    chore: concluid prepare on commit

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -56,7 +56,6 @@ export const Repository = () => {
                     }
                 })
             ]);
-
             setRepositoryDetail(repositoryData.data)
             setRepositoryIssues(issuesData.data)
             setIsLoading(false);
